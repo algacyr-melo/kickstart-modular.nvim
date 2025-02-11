9b14f660f03c944c851df494fdca6bba0666a4f1
algacyr-melo <algacyroalga@gmail.com>
2025-02-11 01:38:39 -0300

    change options

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -25,6 +25,9 @@ end)
 
 -- Enable break indent
 vim.opt.breakindent = true
+
+-- Disable line wrap
+vim.opt.wrap = false
 
 -- Save undo history
 vim.opt.undofile = true
